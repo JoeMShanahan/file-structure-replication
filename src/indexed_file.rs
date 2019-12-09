52ed626116e11db2df435d2502c59483a78d1ff4
Joe Shanahan <joe.s.kasc@gmail.com>
2019-12-09 01:27:18 +0000

    Make a fake indexed file and display it

--- src/indexed_file.rs
+++ src/indexed_file.rs
@@ -1,12 +1,27 @@
 use std::path::{Path, PathBuf};
 use std::option::Option::{None};
 use sha2::Sha512;
+use std::fmt;
 
-struct IndexedFile {
+#[derive(Debug)]
+pub struct IndexedFile {
     path: PathBuf,
     hash: Sha512
 }
 
-fn processFile(file: &Path) -> Option<IndexedFile> {
-  return None;
+impl fmt::Display for IndexedFile {
+  fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+      write!(f, "IndexedFile{{ path: {:#?}, hash: {:#?} }}", self.path, self.hash)
+  }
+}
+
+pub fn process_file(file: &Path) -> Option<IndexedFile> {
+    let path = file.to_path_buf();
+    let hash = hash_file(file)?;
+    let indexed_file = IndexedFile { path, hash };
+    return Some(indexed_file);
+}
+
+fn hash_file (_file: &Path) -> Option<Sha512> {
+    return Some(Sha512::default());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::path::Path;
+mod indexed_file;
+
 fn main() {
+    let indexed_file = indexed_file::process_file(Path::new("./nope.txt"));
+    println!("{:#?}", indexed_file);
     println!("Hello, world!");
 }
