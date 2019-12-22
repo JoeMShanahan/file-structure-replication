d9260fbecf8cca0bd733f71704ef2f554229dd4b
Joe Shanahan <joe.s.kasc@gmail.com>
2019-12-22 02:15:17 +0000

    Move files into own folders

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -5,8 +5,9 @@ use std::path::Path;
 #[derive(Debug)]
 pub struct Hash {
     hash: Sha512,
-}
+}   
 
 pub fn hash_file (_file: &Path) -> Option<Hash> {
     return Some(Hash{hash: Sha512::default()});
 }
+
